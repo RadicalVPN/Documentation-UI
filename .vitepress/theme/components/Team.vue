2d7c23109c10741e4a20b064c3721bd848e2ab7e
Primexz <johann@pepebot.info>
2024-01-18 18:14:19 +0100

    chore: remove essbo from team component

--- .vitepress/theme/components/Team.vue
+++ .vitepress/theme/components/Team.vue
@@ -1,40 +1,20 @@
 <template>
   <VPTeamPageTitle>
-    <template #title>
-      Our Team
-    </template>
-    <template #lead>
-        RadicalVPN is developed by a small, German developer team
-    </template>
+    <template #title> Our Team </template>
+    <template #lead> RadicalVPN is developed by a small, German developer team </template>
   </VPTeamPageTitle>
-  <VPTeamMembers
-    :members="members"
-  />
+  <VPTeamMembers :members="members" />
 </template>
 
 <script setup>
-import {
-  VPTeamPage,
-  VPTeamPageTitle,
-  VPTeamMembers
-} from 'vitepress/theme'
+  import { VPTeamPage, VPTeamPageTitle, VPTeamMembers } from 'vitepress/theme'
 
-const members = [
-  {
-    avatar: 'https://avatars.githubusercontent.com/u/76482511?v=4',
-    name: 'Prime',
-    title: 'Creator, Main Developer',
-    links: [
-      { icon: 'github', link: 'https://github.com/Primexz' }
-    ]
-  },
+  const members = [
     {
-    avatar: 'https://avatars.githubusercontent.com/u/73748381?v=4',
-    name: 'Essbo',
-    title: 'Infrastructure',
-    links: [
-      { icon: 'github', link: 'https://github.com/essbo' }
-    ]
-  }
-]
+      avatar: 'https://avatars.githubusercontent.com/u/76482511?v=4',
+      name: 'Prime',
+      title: 'Creator, Main Developer',
+      links: [{ icon: 'github', link: 'https://github.com/Primexz' }],
+    },
+  ]
 </script>
